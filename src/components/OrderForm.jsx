de479ea2d4207e814ee85e19df0f258ca98d1a34
Lockett Pundt <kalifornium@hotmail.com>
2020-07-08 12:11:01 -0400

    added calendar, select menu for time, imported the modal for the terms page, passed modal component the setmodalstatus to close modal with a key or click, checkbox for terms agreement.

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import {
-  Box, Form, CheckBoxGroup, Button, TextInput,
+  Box, Form, CheckBoxGroup, Button, TextInput, CheckBox, Calendar, Text, Anchor, Select
 } from 'grommet';
 import axios from 'axios';
 import API_URL from '../utils/apiconn';
+import TermsModal from './TermsModal';
 
 const OrderForm = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [services, setServices] = useState([]);
-  const [jobDate, setjobDate] = useState('1/1/1922');
+  const [jobDate, setjobDate] = useState('');
+  const [acceptTerms, setAcceptTerms] = useState(false);
+  const [modalStatus, setModalStatus] = useState(false);
+  const [time, setTime] = useState('');
 
   const options = [
     'Drone Pictures',
@@ -22,6 +26,9 @@ const OrderForm = () => {
     'Video',
     'Virtual Tour',
   ];
+  const selectOptions = [
+    "9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00"
+  ]
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -31,47 +38,100 @@ const OrderForm = () => {
       email,
       dateCreated: new Date(),
       jobDate,
-      services,
-    };
+      time,
+      acceptTerms,
+      services
+    }
     const response = axios.post(API_URL, jobInfo).then((data) => console.log(data));
   };
 
   return (
     <Box
+      responsive
       width="medium"
-      margin="auto"
+      margin={{
+        horizontal: "auto",
+        top: "20px",
+        bottom: "20px"
+      }}
+      elevation="small"
+      pad="small"
     >
+      {modalStatus
+        ? <TermsModal setModalStatus={setModalStatus} />
+        : null
+      }
       <Form onSubmit={(e) => handleSubmit(e)}>
-        <TextInput
-          placeholder="First Name"
-          required
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-        <TextInput
-          margin="small"
-          placeholder="Last Name"
-          value={lastName}
-          required
-          onChange={(e) => setLastName(e.target.value)}
-        />
-        <TextInput
-          margin="small"
-          placeholder="Email"
-          value={email}
-          required
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <Box
+          height="small"
+          justify="evenly"
+        >
+          <TextInput
+            placeholder="First Name"
+            required
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <TextInput
+            margin="small"
+            placeholder="Last Name"
+            value={lastName}
+            required
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <TextInput
+            margin="small"
+            placeholder="Email"
+            value={email}
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Box>
         <CheckBoxGroup
           margin="small"
           options={options}
           onChange={(e) => setServices(e.value)}
         />
-        <Button
-          type="submit"
-          label="Submit"
-          alignSelf="center"
+        <Calendar
+          margin="small"
+          size="small"
+          animate
+          required
+          onSelect={(e) => setjobDate(e)}
         />
+        <Select
+          required
+          margin="small"
+          options={selectOptions}
+          value={time}
+          onChange={({ option }) => setTime(option)}
+        />
+        <Box
+          margin="small"
+        >
+          <Text>
+            <Anchor
+              label="Click "
+              onClick={() => setModalStatus(true)}
+            />
+         to view terms.
+        </Text>
+          <CheckBox
+            margin="small"
+            required
+            checked={acceptTerms}
+            onChange={(e) => setAcceptTerms(e.target.checked)}
+            label="Agree to Terms"
+          />
+        </Box>
+        <Box>
+          <Button
+            primary
+            margin="auto"
+            type="submit"
+            label="Submit"
+          />
+        </Box>
       </Form>
     </Box>
   );
