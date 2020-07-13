3ca176fa51fe10dd4dd42f598552128d6a485dad
Lockett Pundt <kalifornium@hotmail.com>
2020-07-13 17:39:08 -0400

    imported Main component. Imported MainTheme and placed in grommet provider. placed Main component in home route.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import React from 'react';
 import { Grommet } from 'grommet';
 import OrderForm from './components/OrderForm';
+import Main from './components/Main';
 import CompletedOrder from './components/CompletedOrder';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 import API_URL from './utils/apiconn';
+import { MainTheme}  from './themes/MainTheme';
 
 const client = new ApolloClient({
   uri: API_URL,
@@ -16,8 +18,9 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <Grommet>
-            <Route exact path="/" component={OrderForm} />
+        <Grommet theme={MainTheme}>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/createorder" component={OrderForm} />
             <Route exact path="/order/:id?" component={CompletedOrder} />
         </Grommet>
       </Router>
