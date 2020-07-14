8e532bbd338c458f838c3d40b4edf8cfd2708453
Lockett Pundt <kalifornium@hotmail.com>
2020-07-14 16:08:19 -0400

    linter changes. nothing different other than spacing.

--- src/components/TermsModal.jsx
+++ src/components/TermsModal.jsx
@@ -2,29 +2,24 @@ import React from 'react';
 import { Layer, Box, Image } from 'grommet';
 import Terms from '../assets/Terms.png';
 
-const TermsModal = ({ setModalStatus }) => {
-
-
-  return (
-    <Layer
-      animate
-      modal
-      onClick={() => setModalStatus(false)}
-      onKeyDown={() => setModalStatus(false)}
+const TermsModal = ({ setModalStatus }) => (
+  <Layer
+    animate
+    modal
+    onClick={() => setModalStatus(false)}
+    onKeyDown={() => setModalStatus(false)}
+  >
+    <Box
+      width="large"
+      overflow="scroll"
+      margin-top="medium"
     >
-      <Box
-        width="large"
-        overflow="scroll"
-        margin-top="medium"
-      >
-        <Image
-          fill
-          src={Terms}
-        />
-      </Box>
-    </Layer>
-  );
-};
-
+      <Image
+        fill
+        src={Terms}
+      />
+    </Box>
+  </Layer>
+);
 
 export default TermsModal;
