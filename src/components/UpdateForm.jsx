08d6e4a73d1379845a8d974ed399481b7b278a14
Lockett Pundt <kalifornium@hotmail.com>
2020-07-14 16:01:01 -0400

    placeholder for update form. This will eventually poulate a form with the order's info to edit.

---
+++ src/components/UpdateForm.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Box, Text } from 'grommet';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_ORDER } from '../queries/queries';
+
+
+// initial version. making it so the user can update the post. 
+// the info will be filled into a form and it can be edited. 
+const UpdateForm = () => {
+  const { confirmationNum } = useParams()
+  const { loading, error, data } = useQuery(GET_ORDER, {
+    variables: { id: confirmationNum },
+  });
+
+  if (loading) return 'Preparing your order...';
+  if (error) return `ERROR! ${error.message}`;
+
+  const {
+    email, firstName, lastName, jobDate, services, time, id,
+  } = data.singleOrder;
+  return (
+    <Box>
+      <Text>
+        You should receive an email shortly with your
+        order information.
+      </Text>
+      <Text>
+        Confirmation Number:
+        {' '}
+        {id}
+      </Text>
+      <Text>
+        {firstName}
+        {' '}
+        {lastName}
+      </Text>
+      <Text>
+        {email}
+      </Text>
+      <Text>
+        {services.map((item) => (
+          <Text key={item}>
+            {item}
+            {', '}
+          </Text>
+        ))}
+      </Text>
+    </Box>
+  );
+};
+
+export default UpdateForm;
