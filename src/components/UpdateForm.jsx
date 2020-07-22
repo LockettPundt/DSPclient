aafca748ff8961dfa07a5caa4a3f37cff906a4a1
Lockett Pundt <kalifornium@hotmail.com>
2020-07-22 15:06:00 -0400

    the component makes an API call to retrieve the order info and places it into the form for updating. I'll make a new update mutation query for the submission.

--- src/components/UpdateForm.jsx
+++ src/components/UpdateForm.jsx
@@ -1,51 +1,150 @@
-import React from 'react';
-import { Box, Text } from 'grommet';
+import React, { useState } from 'react';
+import { Box, TextInput, Form, CheckBoxGroup, Button, Text } from 'grommet';
 import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { GET_ORDER } from '../queries/queries';
+//import { ADD_ORDER } from '../queries/mutations';
+import { useHistory } from 'react-router-dom';
 
 
-// initial version. making it so the user can update the post. 
-// the info will be filled into a form and it can be edited. 
 const UpdateForm = () => {
   const { confirmationNum } = useParams()
   const { loading, error, data } = useQuery(GET_ORDER, {
     variables: { id: confirmationNum },
   });
+  const [updateFirstName, setUpdateFirstName] = useState('');
+  const [updateLastName, setUpdateLastName] = useState('');
+  const [updateEmail, setUpdateEmail] = useState('');
+  const [updateServices, setUpdateServices] = useState([]);
+  const [updateJobDate, setUpdateJobDate] = useState('');
+  const [updateTime, setUpdateTime] = useState('');
+  const [updatePhone, setUpdatePhone] = useState('');
+  const history = useHistory();
+  // const [addOrder] = useMutation(ADD_ORDER);
 
-  if (loading) return 'Preparing your order...';
-  if (error) return `ERROR! ${error.message}`;
+  const options = [
+    'Drone Pictures',
+    'Listing Video',
+    'Photography',
+    'Twilight Shoot',
+    'Web Gallery',
+    'Video',
+    'Virtual Tour',
+  ];
 
-  const {
-    email, firstName, lastName, jobDate, services, time, id,
-  } = data.singleOrder;
+  if (loading) {
+    return (
+      <h3>
+        FETCHING ORDER....
+      </h3>
+    )
+  }
+
+  if (error) {
+    return (
+      <h3>
+        Sorry. There was an error processing your request.
+        {error.message}
+      </h3>
+    )
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const jobInfo = {
+      firstName: updateFirstName,
+      lastName: updateLastName,
+      email: updateEmail,
+      dateCreated: new Date(),
+      jobDate: updateJobDate,
+      time: updateTime,
+      acceptTerms: true,
+      services: updateServices,
+      phone: updatePhone,
+    };
+    console.log(jobInfo);
+  }
+
+  const { email, firstName, lastName, id, services, jobDate, time, phone } = data.singleOrder;
+
   return (
     <Box>
       <Text>
-        You should receive an email shortly with your
-        order information.
+        Confirmation Number {id}
       </Text>
-      <Text>
-        Confirmation Number:
-        {' '}
-        {id}
-      </Text>
-      <Text>
-        {firstName}
-        {' '}
-        {lastName}
-      </Text>
-      <Text>
-        {email}
-      </Text>
-      <Text>
-        {services.map((item) => (
-          <Text key={item}>
-            {item}
-            {', '}
-          </Text>
-        ))}
-      </Text>
+      <Form onSubmit={(e) => handleSubmit(e)}>
+        <Box
+          height="small"
+          justify="evenly"
+        >
+          <TextInput
+            placeholder="First Name"
+            required
+            value={updateFirstName ? updateFirstName : firstName}
+            onChange={(e) => setUpdateFirstName(e.target.value)}
+          />
+          <TextInput
+            margin="small"
+            placeholder="Last Name"
+            value={updateLastName ? updateLastName : lastName}
+            required
+            onChange={(e) => setUpdateLastName(e.target.value)}
+          />
+          <TextInput
+            margin="small"
+            placeholder="Email"
+            value={updateEmail ? updateEmail : email}
+            required
+            onChange={(e) => setUpdateEmail(e.target.value)}
+          />
+          <TextInput
+            margin="small"
+            type="tel"
+            placeholder="Phone Number"
+            value={updatePhone ? updatePhone : phone}
+            required
+            pattern="[0-9]{10}"
+            onChange={(e) => setUpdatePhone(e.target.value)}
+          />
+        </Box>
+        <CheckBoxGroup
+          margin="small"
+          options={options}
+          value={services}
+          onChange={(e) => setUpdateServices(e.value)}
+        />
+        <Box
+          height="small"
+          justify="evenly"
+        >
+          < TextInput
+            type="date"
+            animate
+            value={updateJobDate ? updateJobDate : jobDate}
+            required
+            onChange={(e) => setUpdateJobDate(e.target.value)}
+          />
+          < TextInput
+            type="time"
+            required
+            value={updateTime ? updateTime : time}
+            margin="small"
+            onChange={(e) => setUpdateTime(e.target.value)}
+          />
+        </Box>
+        <Box
+          margin={{
+            vertical: 'large',
+          }}
+        >
+          <Button
+            primary
+            margin="auto"
+            type="submit"
+            label="Update"
+          />
+        </Box>
+      </Form>
     </Box>
   );
 };
